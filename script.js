c32d041f3a9e4c4bb1d2e113bbaea147521a71d1
Joshua Garcia <112138421+garciajv86@users.noreply.github.com>
2025-05-01 14:43:17 -0500

    added the form to the aside bar and toggle feature so if type cycling is chosen then cadence is switch to elevation gain and vise versa

--- script.js
+++ script.js
@@ -11,6 +11,8 @@ const inputDuration = document.querySelector(".form__input--duration");
 const inputCadence = document.querySelector(".form__input--cadence");
 const inputElevation = document.querySelector(".form__input--elevation");
 
+let map, mapEvent;
+
 if (navigator.geolocation)
   navigator.geolocation.getCurrentPosition(
     function (position) {
@@ -18,7 +20,7 @@ if (navigator.geolocation)
 
       const coords = [latitude, longitude];
 
-      const map = L.map("map").setView(coords, 13);
+      map = L.map("map").setView(coords, 13);
 
       L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
         attribution:
@@ -30,33 +32,55 @@ if (navigator.geolocation)
         .bindPopup("A pretty CSS popup.<br> Easily customizable.")
         .openPopup();
 
-      map.on("click", function (mapEvent) {
-        const { lat, lng } = mapEvent.latlng;
-        const mapCoords = [lat, lng];
-
-        //* Options for the marker method
-        const markerOptions = {
-          riseOnHover: true,
-          draggable: true,
-        };
-
-        //* Options for the popup method
-        const popupOptions = {
-          maxWidth: 250,
-          minWidth: 100,
-          autoClose: false,
-          closeOnClick: false,
-          className: "running-popup",
-          content: "Workout",
-        };
-
-        L.marker(mapCoords, markerOptions)
-          .addTo(map)
-          .bindPopup(L.popup(popupOptions))
-          .openPopup();
+      //* Handling clicks on map
+      map.on("click", function (mapE) {
+        mapEvent = mapE;
+        form.classList.remove("hidden");
+        inputDistance.focus();
       });
     },
     function () {
       alert("Could not get your position");
     }
   );
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  //* Clear input fields
+  inputDistance.value =
+    inputDuration.value =
+    inputCadence.value =
+    inputElevation.value =
+      "";
+
+  //* Display marker
+  const { lat, lng } = mapEvent.latlng;
+  const mapCoords = [lat, lng];
+
+  //* Options for the marker method
+  const markerOptions = {
+    riseOnHover: true,
+    draggable: true,
+  };
+
+  //* Options for the popup method
+  const popupOptions = {
+    maxWidth: 250,
+    minWidth: 100,
+    autoClose: false,
+    closeOnClick: false,
+    className: "running-popup",
+    content: "Workout",
+  };
+
+  L.marker(mapCoords, markerOptions)
+    .addTo(map)
+    .bindPopup(L.popup(popupOptions))
+    .openPopup();
+});
+
+inputType.addEventListener("change", function () {
+  inputElevation.closest(".form__row").classList.toggle("form__row--hidden");
+  inputCadence.closest(".form__row").classList.toggle("form__row--hidden");
+});
