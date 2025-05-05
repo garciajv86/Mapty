ed20a5ff4a948ecdce65913e4ed8a53b1f7e7772
Joshua Garcia <112138421+garciajv86@users.noreply.github.com>
2025-05-05 12:05:24 -0500

    Added the Class's Workout, Running, Cylcing, where Workout is the parent Class of the other 2, also added some methods on the classes as well as a couple public properties on the parent class

--- script.js
+++ script.js
@@ -13,6 +13,57 @@ const inputElevation = document.querySelector(".form__input--elevation");
 
 let map, mapEvent;
 
+class Workout {
+  date = new Date();
+  id =
+    Date.now().toString() +
+    Math.floor(Math.random() * 1000000)
+      .toString()
+      .padStart(6, "0"); // Using a library to get a unique ID is actually a better practice
+
+  constructor(coords, distance, duration) {
+    this.coords = coords; // [lat, lng]
+    this.distance = distance; // in km
+    this.duration = duration; // in min
+  }
+}
+
+class Running extends Workout {
+  constructor(coords, distance, duration, cadence) {
+    super(coords, distance, duration);
+    this.cadence = cadence;
+    this.calcPace();
+  }
+
+  calcPace() {
+    // min/km
+    this.pace = this.duration / this.distance;
+    return this.pace;
+  }
+}
+
+class Cycling extends Workout {
+  constructor(coords, distance, duration, elevationGain) {
+    super(coords, distance, duration);
+    this.elevationGain = elevationGain;
+    this.calcSpeed();
+  }
+
+  calcSpeed() {
+    // km/h
+    this.speed = this.distance / (this.duration / 60);
+    return this.speed;
+  }
+}
+
+// const run1 = new Running([39, -12], 5.2, 24, 178);
+// const cycling1 = new Cycling([39, -12], 27, 95, 523);
+
+console.log(run1);
+console.log(cycling1);
+
+////////////////////////////////////////////////////////////
+//* APPLICATION ARCHITECTURE
 class App {
   #map;
   #mapEvent;
